c92004c9a186b2a7d38adcdd0c423429cc92b2ff
adam <afenton@getmoxied.net>
2016-04-12 17:57:39 -0500

    Change trim fallback

--- src/Strings.php
+++ src/Strings.php
@@ -23,6 +23,6 @@ class Strings
 
 		$wrapped_text = explode( '\n', wordwrap( $text , $char_limit, '\n' ) );
 
-		return is_array( $wrapped_text ) ? $wrapped_text[0] : $text;
+		return is_array( $wrapped_text ) ? $wrapped_text[0] : substr( $text, 0, $char_limit );
 	}
 }
